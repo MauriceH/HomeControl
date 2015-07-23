672dca1e3c4e581f4fcc683f46c79f134a0a1bbd
Maurice H <m.hessing@validdata.de>
2015-07-23 13:32:32 +0200

    Startmenu activity added + style changes

--- app/src/main/java/de/maurice144/homecontrol/FrontEnd/StartActivity.java
+++ app/src/main/java/de/maurice144/homecontrol/FrontEnd/StartActivity.java
@@ -1,10 +1,15 @@
 package de.maurice144.homecontrol.FrontEnd;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
+import de.maurice144.homecontrol.MainControlActivity;
 import de.maurice144.homecontrol.R;
 
 public class StartActivity extends ActionBarActivity {
@@ -13,6 +18,26 @@ public class StartActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        ImageButton button = (ImageButton)findViewById(R.id.startscreen_menu_control);
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return false;
+            }
+        });
+
+
+
+
     }
+
+
+    public void onMenu_StartControl_Click(View v) {
+        startActivity(new Intent(this, MainControlActivity.class));
+    }
+
+
 
 }
